04a56acffae49f755c9f95925c46e4e95fb036ba
Clara Oliveira <102081296+ClaraOMello@users.noreply.github.com>
2022-06-18 13:39:02 -0300

    Add files via upload
    Arquivos da parte template adicionados

---
+++ codigo/scriptModeloLinguagem.js
@@ -0,0 +1,251 @@
+//json
+const conteudos = [
+    {
+        linguagem: "C",
+        imagem: "images/miniCardC.png",
+        topicos: [
+            {
+                topico: "Introdução",
+                autor: "EasyCode",
+                conteudo: "<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Mauris pharetra et ultrices neque ornare aenean euismod. Nunc mattis enim ut tellus. Dignissim diam quis enim lobortis scelerisque fermentum dui faucibus in. Amet aliquam id diam maecenas ultricies mi. Consectetur libero id faucibus nisl tincidunt eget nullam non nisi. Nam at lectus urna duis convallis convallis. Donec pretium vulputate sapien nec. Placerat duis ultricies lacus sed turpis tincidunt. Donec et odio pellentesque diam volutpat commodo. Enim nulla aliquet porttitor lacus. Aliquam eleifend mi in nulla. Iaculis nunc sed augue lacus viverra vitae congue eu consequat. Erat pellentesque adipiscing commodo elit at imperdiet dui accumsan. Cras pulvinar mattis nunc sed blandit libero volutpat.</p>",
+                exemplo: [
+                    {
+                        texto: "",
+                        imagem: "images/banner.png",
+                    }
+                ],
+                exercicios: "<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Mauris pharetra et ultrices neque ornare aenean euismod. Nunc mattis enim ut tellus. Dignissim diam quis enim lobortis scelerisque fermentum dui faucibus in. Amet aliquam id diam maec</p>",
+                id: "lingCtop1"
+            },
+            {
+                topico: "Vetores",
+                autor: "EasyCode",
+                conteudo: "<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Mauris pharetra et ultrices neque ornare aenean euismod. Nunc mattis enim ut uis convallis convallis. Donec pretium vulputate sapien nec. Placerat duis ultricies lacus sed turpis tincidunt. Donec et odio pellentesque diam volutpat commodo. Enim nulla aliquet porttitor lacus. Aliquam eleifend mi in nulla. Iaculis nunc sed augue lacus viverra vitae congue eu consequat. Erat pellentesque adipiscing commodo elit at imperdiet dui accumsan. Cras pulvinar mattis nunc sed blandit libero volutpat.</p>",
+                exemplo: [
+                    {
+                        texto: "<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Mauris pharetra et ultrices neque ornare aenean euismod. Nunc mattis enim ut uis convallis convallis. Donec pretium vulputate sapien nec. Placerat duis ultricies lacus sed turpis tincidunt. Donec et odio pellentesque diam volutpat commodo.</p>",
+                        imagem: "",
+                    }
+                ],
+                exercicios: "",
+                id: "lingCtop2"
+            }
+        ],
+        id: "lingC"
+    },
+    {
+        linguagem: "CSS",
+        imagem: "images/miniCardCSS.png",
+        topicos: [
+            {
+                topico: "Introdução",
+                autor: "EasyCode",
+                conteudo: "<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Mauris pharetra et ultrices neque ornare aenean euismod. Nunc mattis enim ut tellus. Dignissim diam quis enim lobortis scelerisque fermentum dui faucibus in. Amet aliquam id diam maecenas ultricies mi. Consectetur libero id faucibus nisl tincidunt eget nullam non nisi. Nam at lectus urna duis convallis convallis. Erat pellentesque adipiscing commodo elit at imperdiet dui accumsan. Cras pulvinar mattis nunc sed blandit libero volutpat.</p>",
+                exemplo: "",
+                exercicios: "<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Mauris pharetra et ultrices neque ornare aenean euismod. Nunc mattis enim ut tellus. Dignissim diam quis enim lobortis scelerisque fermentum dui faucibus in. Amet aliquam id diam maec</p>",
+                id: "lingCtop1"
+            }
+        ],
+        id: "lingCSS"
+    },
+    {
+        linguagem: "C#",
+        imagem: "images/miniCardCSharp.png",
+        topicos: [
+            {
+                topico: "Introdução",
+                autor: "EasyCode",
+                conteudo: "<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Mauris pharetra et ultrices neque ornare aenean euismod. Nunc mattis enim ut tellus. Dignissim diam quis enim lobortis scelerisque fermentum dui faucibus in. Amet aliquam id diam maecenas ultricies mi. Consectetur libero id faucibus nisl tincidunt eget nullam non nisi. Nam at lectus urna duis convallis convallis. Erat pellentesque adipiscing commodo elit at imperdiet dui accumsan. Cras pulvinar mattis nunc sed blandit libero volutpat.</p>",
+                exemplo: "",
+                exercicios: "<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Mauris pharetra et ultrices neque ornare aenean euismod. Nunc mattis enim ut tellus. Dignissim diam quis enim lobortis scelerisque fermentum dui faucibus in. Amet aliquam id diam maec</p>",
+                id: "lingCtop1"
+            }
+        ],
+        id: "lingCSharp"
+    },
+    {
+        linguagem: "C++",
+        imagem: "images/miniCardC++.png",
+        topicos: [
+            {
+                topico: "Introdução",
+                autor: "EasyCode",
+                conteudo: "<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Mauris pharetra et ultrices neque ornare aenean euismod. Nunc mattis enim ut tellus. Dignissim diam quis enim lobortis scelerisque fermentum dui faucibus in. Amet aliquam id diam maecenas ultricies mi. Consectetur libero id faucibus nisl tincidunt eget nullam non nisi. Nam at lectus urna duis convallis convallis. Erat pellentesque adipiscing commodo elit at imperdiet dui accumsan. Cras pulvinar mattis nunc sed blandit libero volutpat.</p>",
+                exemplo: "",
+                exercicios: "<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Mauris pharetra et ultrices neque ornare aenean euismod. Nunc mattis enim ut tellus. Dignissim diam quis enim lobortis scelerisque fermentum dui faucibus in. Amet aliquam id diam maec</p>",
+                id: "lingCtop1"
+            }
+        ],
+        id: "lingCpp"
+    },
+    {
+        linguagem: "HTML",
+        imagem: "images/miniCardHTML.png",
+        topicos: [
+            {
+                topico: "Introdução",
+                autor: "EasyCode",
+                conteudo: "<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Mauris pharetra et ultrices neque ornare aenean euismod. Nunc mattis enim ut tellus. Dignissim diam quis enim lobortis scelerisque fermentum dui faucibus in. Amet aliquam id diam maecenas ultricies mi. Consectetur libero id faucibus nisl tincidunt eget nullam non nisi. Nam at lectus urna duis convallis convallis. Erat pellentesque adipiscing commodo elit at imperdiet dui accumsan. Cras pulvinar mattis nunc sed blandit libero volutpat.</p>",
+                exemplo: [
+                    {
+                        texto: "<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Mauris pharetra et ultrices neque ornare aenean euismod. Nunc mattis enim ut uis convallis convallis. Donec pretium vulputate sapien nec. Placerat duis ultricies lacus sed turpis tincidunt. Donec et odio pellentesque diam volutpat commodo.</p>",
+                        imagem: "images/exemplo.png",
+                    }
+                ],
+                exercicios: "<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Mauris pharetra et ultrices neque ornare aenean euismod. Nunc mattis enim ut tellus. Dignissim diam quis enim lobortis scelerisque fermentum dui faucibus in. Amet aliquam id diam maec</p>",
+                id: "lingCtop1"
+            }
+        ],
+        id: "lingHTML"
+    },
+    {
+        linguagem: "Python",
+        imagem: "images/miniCardPython.png",
+        topicos: [
+            {
+                topico: "Introdução",
+                autor: "EasyCode",
+                conteudo: "<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Mauris pharetra et ultrices neque ornare aenean euismod. Nunc mattis enim ut tellus. Dignissim diam quis enim lobortis scelerisque fermentum dui faucibus in. Amet aliquam id diam maecenas ultricies mi. Consectetur libero id faucibus nisl tincidunt eget nullam non nisi. Nam at lectus urna duis convallis convallis. Erat pellentesque adipiscing commodo elit at imperdiet dui accumsan. Cras pulvinar mattis nunc sed blandit libero volutpat.</p>",
+                exemplo: "",
+                exercicios: "<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Mauris pharetra et ultrices neque ornare aenean euismod. Nunc mattis enim ut tellus. Dignissim diam quis enim lobortis scelerisque fermentum dui faucibus in. Amet aliquam id diam maec</p>",
+                id: "lingCtop1"
+            }
+        ],
+        id: "lingPython"
+    },
+    {
+        linguagem: "JAVA",
+        imagem: "images/miniCardJAVA2.png",
+        topicos: [
+            {
+                topico: "Introdução",
+                autor: "EasyCode",
+                conteudo: "<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Mauris pharetra et ultrices neque ornare aenean euismod. Nunc mattis enim ut tellus. Dignissim diam quis enim lobortis scelerisque fermentum dui faucibus in. Amet aliquam id diam maecenas ultricies mi. Consectetur libero id faucibus nisl tincidunt eget nullam non nisi. Nam at lectus urna duis convallis convallis. Erat pellentesque adipiscing commodo elit at imperdiet dui accumsan. Cras pulvinar mattis nunc sed blandit libero volutpat.</p>",
+                exemplo: "",
+                exercicios: "<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Mauris pharetra et ultrices neque ornare aenean euismod. Nunc mattis enim ut tellus. Dignissim diam quis enim lobortis scelerisque fermentum dui faucibus in. Amet aliquam id diam maec</p>",
+                id: "lingCtop1"
+            }
+        ],
+        id: "lingJAVA"
+    },
+    {
+        linguagem: "JavaScript",
+        imagem: "images/miniCardJavaScript.png",
+        topicos: [
+            {
+                topico: "Introdução",
+                autor: "EasyCode",
+                conteudo: "<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Mauris pharetra et ultrices neque ornare aenean euismod. Nunc mattis enim ut tellus. Dignissim diam quis enim lobortis scelerisque fermentum dui faucibus in. Amet aliquam id diam maecenas ultricies mi. Consectetur libero id faucibus nisl tincidunt eget nullam non nisi. Nam at lectus urna duis convallis convallis. Erat pellentesque adipiscing commodo elit at imperdiet dui accumsan. Cras pulvinar mattis nunc sed blandit libero volutpat.</p>",
+                exemplo: "",
+                exercicios: "<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Mauris pharetra et ultrices neque ornare aenean euismod. Nunc mattis enim ut tellus. Dignissim diam quis enim lobortis scelerisque fermentum dui faucibus in. Amet aliquam id diam maec</p>",
+                id: "lingCtop1"
+            }
+        ],
+        id: "lingJavaScript"
+    }
+]
+
+/* ------------------------------------------------ */
+
+let txtLinguagem = '';
+
+let params = new URLSearchParams(location.search);
+let ID = params.get('id');
+let ling = params.get('principal');
+
+onload = () => {
+    
+    for (let i = 0; i < conteudos.length; i++)
+    {
+        if(conteudos[i].id == ling)
+        {
+            for (let j = 0; j < conteudos[i].topicos.length; j++)
+            {
+                if(conteudos[i].topicos[j].id == ID)
+                {                    
+                    txtLinguagem += `<div class="tituloModeloLing">
+                    <h1>${conteudos[i].linguagem} - <span>${conteudos[i].topicos[j].topico}</span></h1>
+                    </div>`;
+                
+                    txtLinguagem += `<div class="conteudoModeloLing">${conteudos[i].topicos[j].conteudo}</div>`;
+                    if (conteudos[i].topicos[j].exemplo != '')
+                    {
+                        if (conteudos[i].topicos[j].exemplo[0].imagem != '' && conteudos[i].topicos[j].exemplo[0].texto != '')
+                        {
+                            txtLinguagem += `<div class="exemploModeloLing">
+                            <h3 >Exemplo</h3>
+                            <div class="row">
+                            <span class="col-12 col-lg-7 col-xl-7">${conteudos[i].topicos[j].exemplo[0].texto}</span>
+                            <img src="${conteudos[i].topicos[j].exemplo[0].imagem}" alt="representacao do codigo" class="col-12 col-lg-5 col-xl-5">
+                            </div>
+                            </div>`;
+                        }
+                        else if (conteudos[i].topicos[j].exemplo[0].imagem != '')
+                        {
+                            txtLinguagem += `<div class="exemploModeloLing">
+                            <h3 >Exemplo</h3>
+                            <div class="row">
+                            <img src="${conteudos[i].topicos[j].exemplo[0].imagem}" alt="representacao do codigo">
+                            </div>
+                            </div>`;
+                        }
+                        else if (conteudos[i].topicos[j].exemplo[0].texto != '')
+                        {
+                            txtLinguagem += `<div class="exemploModeloLing">
+                            <h3 >Exemplo</h3>
+                            <div class="row">
+                            ${conteudos[i].topicos[j].exemplo[0].texto}
+                            </div>
+                            </div>`;
+                        }
+                    }
+                    if (conteudos[i].topicos[j].exercicios != '')
+                    {
+                        txtLinguagem += `<div class="exercicioModeloLing">
+                        <h3>Exercícios</h3>
+                        ${conteudos[i].topicos[j].exercicios}
+                        </div>`;
+                    }
+
+                    document.getElementById('areaParaConteudoLing').innerHTML= `${txtLinguagem}`;
+                }
+                /*
+                else
+                {
+                    for (let k = 0; k < conteudos[i].topicos[j].subtopicos.length; k++)
+                    {
+                        if(conteudos[i].topicos[j].subtopicos[j].id == ID)
+                        {
+                            txtLinguagem += `<div class="tituloModeloLing">
+                            <h1>${conteudos[i].linguagem}</h1>
+                            <h3>${conteudos[i].topicos[j].subtopicos[k].subtopico}</h3>
+                            </div>`;
+                        
+                            txtLinguagem += `<div class="conteudoModeloLing">${conteudos[i].topicos[j].subtopicos[k].conteudo}</div>`;
+                            if (conteudos[i].topicos[j].subtopicos[k].exemplo != '')
+                            {
+                                txtLinguagem += `<div class="exemploModeloLing ">
+                                <h3  class="row">Exemplo</h3>
+                                <div class="row">
+                                ${conteudos[i].topicos[j].subtopicos[k].exemplo}
+                                </div>
+                                </div>`;
+                            }
+                            if (conteudos[i].topicos[j].subtopicos[k].exercicios != '')
+                            {
+                                txtLinguagem += `<div class="exercicioModeloLing">
+                                <h3>Exercícios</h3>
+                                ${conteudos[i].topicos[j].subtopicos[k].exercicios}
+                                </div>`;
+                            }
+
+                            document.getElementById('areaParaConteudoLing').innerHTML= `${txtLinguagem}`;
+                        }
+                    }
+                } */ 
+                //retirado os subtopicos ^^^
+            }
+            
+        }
+    }
+}
